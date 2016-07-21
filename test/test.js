e90c733be8e2cc2ad8a42c0ee15d91c7cd7a16e2
Kara de la Marck <karadelamarck@gmail.com>
2016-07-21 16:13:42 +0100

    add more tests for handler endpoints

--- test/test.js
+++ test/test.js
@@ -8,6 +8,22 @@ test("'/' return 200 statusCode", t => {
   });
 });
 
+test("'/createPost' return 201 statusCode", t => {
+  var payload = {post: 'hi'};
+  server.inject({method: 'POST', url: '/createPost', payload}, function (res) {
+    t.equal(res.statusCode, 201, 'post inserted successfully');
+    t.end();
+  });
+});
+
+test("'/getPosts' returns a post", t => {
+  var payload = {post: 'done'};
+  server.inject({method: 'GET', url: '/getPosts', payload}, function (res) {
+    t.deepEqual(payload.post, 'done', 'blog post successfully retrieved a post');
+    t.end();
+  });
+});
+
 test("'/{file*}' returns 200 statusCode", t => {
   server.inject({ method: 'Get', url: '/main.css' },
   res => {
